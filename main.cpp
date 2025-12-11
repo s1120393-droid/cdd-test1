bc2440dfd2d52d3755230aa6d8cea4ea1ceb3b50
s1120393-droid <s1120393@dmhs.kh.edu.tw>
2025-12-11 14:45:28 +0800

    作業9

--- main.cpp
+++ main.cpp
@@ -1,61 +1,35 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-#include <stdio.h>
+#include <iostream>
 
 int main()
 {
-    printf("Hello World");
+    std::cout<<"Hello World";
 
     return 0;
 }#include <iostream>
-#include <cstdlib>   // rand(), srand()
-#include <ctime>     // time()
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int main() {
-    const int N = 10;      // 陣列大小
+int main()
+{
+    int N = 5;
     int arr[N];
 
-    // ---- 亂數種子 ----
     srand(time(NULL));
 
-    cout << "原始亂數陣列: ";
-    for (int i = 0; i < N; i++) {
+    cout << "random number";
+    for (int i=0; i<N; i++) {
         arr[i] = rand() % 100;
         cout << arr[i] << " ";
     }
 
-    cout << endl;
-
-    // ---- 選擇排序 (Selection Sort) ----
-    for (int i = 0; i < N - 1; i++) {
-        int minIndex = i;
-
-        for (int j = i + 1; j < N; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-
-        // 交換
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
-    }
-
-    // ---- 排序後陣列 ----
-    cout << "排序後陣列: ";
-    for (int i = 0; i < N; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
+    for (int i=0; i<N-1; i++) {
+        for (int j=0; j<N-1-i; j++)
